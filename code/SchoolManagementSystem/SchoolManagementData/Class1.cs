9880ecb23993a78907c83e3d9fc4219d1b8f55de
chandra shekhar sengupta <sayan801@gmail.com>
2012-12-28 18:55:11 +0530

    adding list of reports, security, partial structure, estimate

---
+++ code/SchoolManagementSystem/SchoolManagementData/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchoolManagementData
+{
+    public class StudentInfo
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string bloodGroup { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public DateTime admissionDate { get; set; }
+        public DateTime dob { get; set; }
+    }
+
+}
