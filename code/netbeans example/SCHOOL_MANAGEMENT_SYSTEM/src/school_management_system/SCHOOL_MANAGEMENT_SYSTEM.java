20fcc8c38fa495fc8a01f06d0090da6f6ea004da
SUSMITA PODDER <susmitapodder87@gmail.com>
2013-01-19 20:04:22 +0530

    SMS final document and neatbeans GUI added

---
+++ code/netbeans example/SCHOOL_MANAGEMENT_SYSTEM/src/school_management_system/SCHOOL_MANAGEMENT_SYSTEM.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package school_management_system;
+
+/**
+ *
+ * @author user
+ */
+public class SCHOOL_MANAGEMENT_SYSTEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
