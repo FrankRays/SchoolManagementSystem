97609605d2f33f70b1e517b1825ef37a6044a7cc
chandra shekhar sengupta <sayan801@gmail.com>
2013-01-21 01:03:00 +0530

    listview example added

--- code/SusmitaExcercises/SusmitaExcercises/MainWindow.xaml.cs
+++ code/SusmitaExcercises/SusmitaExcercises/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace SusmitaExcercises
 {
@@ -22,6 +23,53 @@ namespace SusmitaExcercises
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ViewModel();  
         }
     }
+
+     public class SubjectAndMarks 
+{
+    public string Subject { get; set; }
+    public double Marks { get; set; }
+}
+public class Student 
+{
+    public string Name { get; set; }
+    public int RollNo { get; set; }
+    public ObservableCollection<SubjectAndMarks> SubjectAndMarks { get; set; }
+}
+
+public class ViewModel //ViewModel
+{
+     public ObservableCollection<Student> Students { get; set; }
+    public ViewModel()
+    {
+        Students = new ObservableCollection<Student>();
+        Students.Add(new Student()
+        {
+            Name = "Harish",
+            RollNo = 1,
+            SubjectAndMarks = new ObservableCollection<SubjectAndMarks>()
+        {new SubjectAndMarks(){Subject="Maths",Marks=100},new SubjectAndMarks(){Subject="Hindi",Marks=100},
+        new SubjectAndMarks(){Subject="Science",Marks=100}}
+        });
+        Students.Add(new Student()
+        {
+            Name = "Pankaj",
+            RollNo = 2,
+            SubjectAndMarks = new ObservableCollection<SubjectAndMarks>()
+        {new SubjectAndMarks(){Subject="Maths",Marks=100},new SubjectAndMarks(){Subject="Hindi",Marks=40},
+        new SubjectAndMarks(){Subject="Science",Marks=30}}
+        });
+        Students.Add(new Student()
+        {
+            Name = "Deepak",
+            RollNo = 3,
+            SubjectAndMarks = new ObservableCollection<SubjectAndMarks>()
+        {new SubjectAndMarks(){Subject="Maths",Marks=90},new SubjectAndMarks(){Subject="Hindi",Marks=50},
+        new SubjectAndMarks(){Subject="Science",Marks=60}}
+        });
+    } 
+}
+   
 }
