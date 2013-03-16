1c01e803eecbb5e5cd6ab5fb2a43057c9c59a456
Amit Paul <amit.cod@gmail.com>
2013-03-16 18:17:40 +0530

    GUI .net sample

---
+++ code/SmsProject/MmmUI/MmmUI/Clients/Clients.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SMSUI
+{
+    /// <summary>
+    /// Interaction logic for Clients.xaml
+    /// </summary>
+    public partial class Clients : UserControl
+    {
+        public Clients()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SMSUI.ViewClient ViewClientobj = new SMSUI.ViewClient();
+
+            clientMainUG.Children.Clear();
+            clientMainUG.Children.Add(ViewClientobj);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SMSUI.AddClient AddClientobj = new SMSUI.AddClient();
+
+            clientMainUG.Children.Clear();
+            clientMainUG.Children.Add(AddClientobj);
+        }
+    }
+}
---
+++ code/SmsProject/MmmUI/MmmUI/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SMSUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SMSUI.Signin Signinobj = new SMSUI.Signin();
+
+            mainUniGrid.Children.Clear();
+            mainUniGrid.Children.Add(Signinobj);
+
+        }
+
+        private void employeesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SMSUI.Employees Employeesobj = new SMSUI.Employees();
+
+            mainUniGrid.Children.Clear();
+            mainUniGrid.Children.Add(Employeesobj);
+
+            
+        }
+
+        private void agentsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SMSUI.Agents Agentsobj = new SMSUI.Agents();
+
+            mainUniGrid.Children.Clear();
+            mainUniGrid.Children.Add(Agentsobj);
+        }
+
+        private void clientsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SMSUI.Clients Clientsobj = new SMSUI.Clients();
+
+            mainUniGrid.Children.Clear();
+            mainUniGrid.Children.Add(Clientsobj);
+        }
+
+        private void reportsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SMSUI.Reports Reportsobj = new SMSUI.Reports();
+
+            mainUniGrid.Children.Clear();
+            mainUniGrid.Children.Add(Reportsobj);
+        }
+
+        private void settingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SMSUI.Settings Settingsobj = new SMSUI.Settings();
+
+            mainUniGrid.Children.Clear();
+            mainUniGrid.Children.Add(Settingsobj);
+        }
+
+        private void helpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SMSUI.Help Helpobj = new SMSUI.Help();
+
+            mainUniGrid.Children.Clear();
+            mainUniGrid.Children.Add(Helpobj);
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
