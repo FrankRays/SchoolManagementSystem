3fb996fedf4488c0b2b9eaa56bdfc19a923fb75b
chandra shekhar sengupta <sayan801@gmail.com>
2013-03-22 09:40:57 +0530

    added proper schooldb

--- code/C#SmsProject/schooldb 22.03.2013 .sql
+++ code/C#SmsProject/schooldb 22.03.2013 .sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.9, for Win32 (x86)
 --
--- Host: localhost    Database: studentdb
+-- Host: localhost    Database: schooldb
 -- ------------------------------------------------------
 -- Server version	5.5.15
 
@@ -16,35 +16,106 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `studentdb`
+-- Current Database: `schooldb`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `studentdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `schooldb` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `studentdb`;
+USE `schooldb`;
 
 --
--- Table structure for table `studenttable`
+-- Table structure for table `admission`
 --
 
-DROP TABLE IF EXISTS `studenttable`;
+DROP TABLE IF EXISTS `admission`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `studenttable` (
-  `roll` int(11) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`roll`)
+CREATE TABLE `admission` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `gurdian` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `applyfor` varchar(45) DEFAULT NULL,
+  `qualification` varchar(45) DEFAULT NULL,
+  `marksprcntg` varchar(45) DEFAULT NULL,
+  `formNo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `studenttable`
+-- Dumping data for table `admission`
 --
 
-LOCK TABLES `studenttable` WRITE;
-/*!40000 ALTER TABLE `studenttable` DISABLE KEYS */;
-INSERT INTO `studenttable` VALUES (2,'hok');
-/*!40000 ALTER TABLE `studenttable` ENABLE KEYS */;
+LOCK TABLES `admission` WRITE;
+/*!40000 ALTER TABLE `admission` DISABLE KEYS */;
+INSERT INTO `admission` VALUES ('41355.3464506597','gfjf','gffg','gj','jgf','jjfg','gf','gfjfg','gjf');
+/*!40000 ALTER TABLE `admission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `guardian` varchar(100) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `dob` datetime DEFAULT NULL,
+  `bloodGroup` varchar(45) DEFAULT NULL,
+  `joinClass` varchar(45) DEFAULT NULL,
+  `schoolFrom` varchar(100) DEFAULT NULL,
+  `joinDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES ('41354.566222419','fglknmfg','fgngj','fghgf','fngfg','2013-03-19 00:00:00','fhfg','fgnh','gfg','2013-03-29 00:00:00'),('41354.7292774306','ss','sss','ss','ss','2013-02-27 00:00:00','ss','sss','sss','2013-03-19 00:00:00');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teacher`
+--
+
+DROP TABLE IF EXISTS `teacher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teacher` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `joinDate` datetime DEFAULT NULL,
+  `tdob` datetime DEFAULT NULL,
+  `address` varchar(145) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `jobId` varchar(45) DEFAULT NULL,
+  `faculty` varchar(45) DEFAULT NULL,
+  `salary` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teacher`
+--
+
+LOCK TABLES `teacher` WRITE;
+/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
+INSERT INTO `teacher` VALUES ('41355.3809493287','amit','2013-04-04 00:00:00','2013-02-24 00:00:00','123','456','a@gmail.com','789','csc','100000');
+/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -56,4 +127,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-03-22  9:10:23
+-- Dump completed on 2013-03-22  9:40:25
